899d199dc601575ce9edfe2c14447a4562d9653a
eduweb <eduweb@eduweb.local>
2019-09-19 11:24:59 +0200

    finish

---
+++ main.rb
@@ -0,0 +1,27 @@
+class Student
+    attr_accessor :name, :last, :age, :email
+    attr_reader :nr_card
+    attr_writer :password
+
+    def initialize(name, last, age, email)
+        @name = name
+        @last = last
+        @age = age
+        @email = email
+        @nr_card = rand.to_s[2..7]
+    end
+    
+    def show
+        "#{name} #{last} #{age} #{email}"
+    end
+    
+    def show_id
+        "#{object_id}, #{name}"
+    end
+end
+
+john = Student.new("John", "Smith", 23, "js@univ.com")
+anna = Student.new("Anna", "Potter", 20, "an@univ.com")
+
+p john.show
+p john.show_idw
